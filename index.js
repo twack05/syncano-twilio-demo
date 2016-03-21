c881fdfa2fe6a65364f0f79ad592e22eebfe09a9
Todd Wacker <toddwacker@Todds-MBP-2.att.net>
2016-03-21 10:03:53 -0500

    Initial commit

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/', function(request, response) {
+	response.render('pages/demo');	
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
